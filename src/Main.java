27c5a2931ef6a084cae1e1a52793c0bf6d378380
thelasttrinh <aarontrinh57@yahoo.com>
2023-07-24 12:15:49 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("JavaFX Application");
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
